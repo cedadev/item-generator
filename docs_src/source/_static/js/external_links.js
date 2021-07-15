33f0160b87aff5661a88255ce9cddb405613894c
rsmith013 <richard.d.smith@stfc.ac.uk>
2021-07-15 21:11:24 +0100

    rename docs source and add auto build action. closes #5

--- docs_src/source/_static/js/external_links.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-   $('a[href^="http://"], a[href^="https://"]').not('a[class*=internal]').attr(
-       {
-          'target':'_blank',
-          'rel': 'noopener noreferrer'
-       }
-       );
-});
